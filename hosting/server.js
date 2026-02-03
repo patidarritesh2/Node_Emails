3d0e2c993682d91b9d13f5b10576f63a5caf821e
patidarritesh2 <patidarritesh2001@gmail.com>
2026-02-03 20:21:11 +0530

    change mail

--- hosting/server.js
+++ hosting/server.js
@@ -9,12 +9,12 @@ console.log(" Starting server...");
 
 // Transporter
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false, // TLS
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
     }
 });
 
